1f6416f7612b974568eda19ee553d564c445dcba
Jamesstahl98 <Jamesstahl556@gmail.com>
2025-02-11 13:13:57 +0100

    Create PokemonCard.js

---
+++ js/PokemonCard.js
@@ -0,0 +1,10 @@
+export class PokemonCard{
+    constructor(id, name, originalPrice, saleInPercent, numberInStock)
+    {
+        this.id = id;
+        this.name = name;
+        this.originalPrice = originalPrice;
+        this.numberInStock = numberInStock;
+        this.saleInPercent = saleInPercent;
+    }
+}
